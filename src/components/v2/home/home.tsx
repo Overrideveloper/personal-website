567aca99f5f8294f78deaebd951c36976f28a083
Overrideveloper <bansowisdom@gmail.com>
2020-04-18 18:51:19 +0200

    feat(core) - Implement sticky nav

--- src/components/v2/home/home.tsx
+++ src/components/v2/home/home.tsx
@@ -6,17 +6,34 @@ import { Component, h } from '@stencil/core';
 })
   
 export class V2Home {
+    componentDidLoad() {
+        window.onscroll = function() { this.fixNavOnScroll() }.bind(this);
+    }
+
+    fixNavOnScroll() {
+        const navbar = document.querySelector('v2-nav') as HTMLV2NavElement;
+        const sticky = navbar.offsetTop;
+
+        if (window.pageYOffset > sticky) {
+            navbar.classList.add('sticky');
+        } else {
+            navbar.classList.remove('sticky');
+        }
+    }
+
     render() {
         return (
             <div class="root">
                 <div class="main">
                     <div class="main-content">
                         <v2-nav></v2-nav>
-                        <v2-hero></v2-hero>
-                        <v2-work></v2-work>
-                        <v2-experience></v2-experience>
-                        <v2-projects></v2-projects>
-                        <v2-future></v2-future>
+                        <v2-content>
+                            <v2-hero></v2-hero>
+                            <v2-work></v2-work>
+                            <v2-experience></v2-experience>
+                            <v2-projects></v2-projects>
+                            <v2-future></v2-future>
+                        </v2-content>
                     </div>
                     
                     <div class="bio">
